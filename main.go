d76448743b735a82d5dae30afb7b8290f4687ee7
神楽坂雅詩 <cyscxchope@gmail.com>
2023-05-23 20:34:11 +0800

    解析图片类型消息并回传图片和附加文本

--- main.go
+++ main.go
@@ -12,44 +12,49 @@ import (
 	"golang.org/x/net/proxy"
 )
 
+type ChatObj struct {
+	ID    int64
+	Title string
+}
+
 func main() {
 
-	fmt.Println("绒布球！")
+	fmt.Println("絨！")
 	socks5 := "socks5://localhost:23332"
 	client := &http.Client{}
 	tgProxyURL, err := url.Parse(socks5)
 	if err != nil {
-		log.Printf("代理服务器地址配置错误: %s\n", err)
+		log.Printf("代理伺服器地址配置錯誤: %s\n", err)
 	}
 	tgDialer, err := proxy.FromURL(tgProxyURL, proxy.Direct)
 	if err != nil {
-		log.Printf("代理服务器错误: %s\n", err)
+		log.Printf("代理伺服器錯誤: %s\n", err)
 	}
 	tgTransport := &http.Transport{
 		Dial: tgDialer.Dial,
 	}
 	client.Transport = tgTransport
 
-	bot, err := tgbotapi.NewBotAPIWithClient(apikey(), "https://api.telegram.org/bot%s/%s", client)
+	bot, err := tgbotapi.NewBotAPIWithClient(apikey, "https://api.telegram.org/bot%s/%s", client)
 	if err != nil {
-		log.Printf("连接电报服务器出现问题: %s\n", err)
+		log.Printf("連線伺服器出現問題: %s\n", err)
 		return
 	}
 
 	bot.Debug = true
 
-	log.Printf("已登录 %s", bot.Self.UserName)
+	log.Printf("已登入 %s", bot.Self.UserName)
 
 	go getUpdates(bot)
 
 	signalch := make(chan os.Signal, 1)
 	signal.Notify(signalch, os.Interrupt, os.Kill)
 	signal := <-signalch
-	fmt.Println("收到系统信号: ", signal)
+	fmt.Println("收到系統訊號: ", signal)
 	if signal == os.Interrupt || signal == os.Kill {
 		bot.StopReceivingUpdates()
 		client.CloseIdleConnections()
-		fmt.Println("终止 BOT")
+		fmt.Println("終止 BOT")
 		os.Exit(0)
 	}
 
@@ -60,20 +65,51 @@ func getUpdates(bot *tgbotapi.BotAPI) {
 	u.Timeout = 60
 	updates := bot.GetUpdatesChan(u)
 	for update := range updates {
-		log.Print(".")
-		if update.Message == nil { // 过滤非消息类型
+		var mode = 0 // 0: 無訊息 1: 文字訊息 2: 圖片訊息 3: 影片訊息 4: 音訊訊息 5: 檔案訊息
+		var msg tgbotapi.Chattable
+		var fromUser ChatObj
+		// var toUser ChatObj
+		// var fromChat ChatObj
+		// var toChat ChatObj
+		var text string = ""
+		// for i := 0; i < updatesLen; i++ {
+		// 	update := updates
+		log.Print(">>>>>>>>>>>>")
+		if update.Message == nil { // 過濾非訊息型別
 			continue
 		}
-
-		log.Printf("收到来自会话 %s(%d) 里 %s(%d) 的消息：%s", update.Message.Chat.UserName, update.Message.Chat.ID, update.Message.From.UserName, update.Message.From.ID, update.Message.Text)
-
-		// msg := tgbotapi.NewEditMessageCaption(update.Message.Chat.ID, update.Message.MessageID, "asasdsad")
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		if _, err := bot.Send(msg); err != nil {
-			log.Printf("发送消息失败: %s", err)
+		log.Printf("收到來自會話 %s(%d) 裡 %s(%d) 的訊息：%s", update.Message.Chat.UserName, update.Message.Chat.ID, update.Message.From.UserName, update.Message.From.ID, update.Message.Text)
+		// log.Println("媒體組: ", update.Message.MediaGroupID)
+		// fromChat = ChatObj{ID: update.Message.Chat.ID, Title: update.Message.Chat.UserName}
+		fromUser = ChatObj{ID: update.Message.From.ID, Title: update.Message.From.UserName}
+		text = update.Message.Text
+		mode = 0
+		var isMediaGroup = len(update.Message.MediaGroupID) > 0
+		var fileID tgbotapi.FileID
+		if update.Message.Photo != nil {
+			fileID = tgbotapi.FileID(update.Message.Photo[0].FileID)
+			println(fileID, update.Message.Caption)
+			mode = 2
+			text = update.Message.Caption
+		} else {
+			mode = 1
 		}
-		log.Printf("已向 %s(%d) 发送消息: %s", update.Message.Chat.UserName, update.Message.Chat.ID, msg.Text)
+		if !isMediaGroup {
+			switch mode {
+			case 1:
+				msg = tgbotapi.NewMessage(testchat, text)
+			case 2:
+				var photoMsg tgbotapi.PhotoConfig = tgbotapi.NewPhoto(testchat, fileID)
+				photoMsg.Caption = text
+				msg = photoMsg
+			default:
+				return
+			}
+			if _, err := bot.Send(msg); err != nil {
+				log.Printf("傳送訊息失敗: %s", err)
+			} else {
+				log.Printf("已向 %s(%d) 傳送訊息: %s", fromUser.Title, fromUser.ID, text)
+			}
+		}
 	}
 }
